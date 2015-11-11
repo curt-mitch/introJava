f446d4283ebb675eb2bf7555ca8fc3b809258a02
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2015-11-10 21:51:50 -0800

    Access control, final keyword

--- FirstProject/src/firstproject/Person.java
+++ FirstProject/src/firstproject/Person.java
@@ -11,51 +11,44 @@ package firstproject;
  */
 public class Person {
 
-    private String firstName;
-    private String lastName;
+    private final String firstName;
+    private final String lastName;
     private int age;
     private Person mother;
+    
+    public Person(String first, String last) {
+        firstName = first;
+        lastName = last;
+        age = 18;
+    }
 
     @Override
     public String toString() {
         return "Person{" + "firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + ", mother=" + mother + '}';
     }
 
-    public Person getMother() {
-        return mother;
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public void setMother(Person mother) {
         this.mother = mother;
-    }
-    
-
-    public Person() { // defined (default) constructor (usually Java adds this itself)
-        age = 18; // age for every new Person will be 18 unless changed later
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public int getAge() {
+        return age;
+    }
+
+    public Person getMother() {
+        return mother;
     }
 
     public String getFullName() {
